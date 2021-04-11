c665544b6fc37d5049fbae4fec19afecc83ae1d1
reisnern <nreisne@wgu.edu>
2021-04-10 20:35:54 -0600

    Adding Javascript elements

---
+++ Assets/js/script.js
@@ -0,0 +1,10 @@
+// define variables for containers
+var Container1 = document.querySelector("header"); 
+var Container2 = document.querySelector("#Container2");
+var Container3 = document.querySelector("#Container3");
+var Container4 = document.querySelector("#Container4"); 
+var Container5 = document.querySelector("#Container5");
+
+
+// quiz start
+var startButton = document.querySelector("#start-button");
