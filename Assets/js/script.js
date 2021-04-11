bcac8ac181f50dc8ef885bdd54a8d51cb223f66c
reisnern <nreisne@wgu.edu>
2021-04-10 21:05:36 -0600

    Added More Javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -26,5 +26,36 @@ ShowQuestion();
 }
 
 function Results() {
-    
+
+    // View & Shield applicable containers
+    Container1.setAttribute("class", "View");
+    Container2.setAttribute("class", "Shield");
+    Container3.setAttribute("class", "Shield");
+    Container4.setAttribute("class", "View");
+    Container5.setAttribute("class", "Shield");
+
+    // View Results and Stop Timer
+    userResults.textContent = timeLeft;
+    countdownTimer.textContent = timeLeft;
+    clearInterval(timeInterval);
+} 
+
+//Timer Function
+
+var countdownTimer = document.querySelector("#countdown");
+var timeLeft = 76;
+var timeInterval;
+
+function startTimer() {
+    timeInterval = setInterval(function () {
+        
+        // Timer count down 
+        timeLeft--;
+        countdownTimer.textContent = timeLeft;
+        
+        // when timer is zero, quiz ends
+        if (timeLeft <= 0) {
+            endQuiz();
+        }
+    }, 1000);
 }
